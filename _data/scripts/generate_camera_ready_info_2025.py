0f63ec57ade9dc50408bab87d5a7449c33350e6b
Jared Strader <jared.l.strader@gmail.com>
2025-06-22 12:16:13 -0700

    update openreview data, remove automated overrides for titles

--- _data/scripts/generate_camera_ready_info_2025.py
+++ _data/scripts/generate_camera_ready_info_2025.py
@@ -27,10 +27,8 @@ import html
 ################################
 #      Paper Overrides
 ################################
-manual_title_overrides = {
-    172: "Particle-Grid Neural Dynamics for Learning Deformable Object Models from RGB-D Videos",
-    215: "A low-cost and lightweight 6 DoF bimanual arm for dynamic and contact-rich manipulation",
-}
+#
+manual_title_overrides = {}
 
 ################################
 #      Handle Latex/Macros
